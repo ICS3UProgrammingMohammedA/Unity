baad8f28b0895d7aaf679e93cb3825110bd7744d
MohammedAlnajeh <43445141+MohammedAlnajeh@users.noreply.github.com>
2018-11-21 16:48:23 -0500

    added stuff on unity

---
+++ SpaceShooterMohammed/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    void Start()
+    {
+       rb  = GetComponent<Rigidbody>(); 
+    }
+    
+
+    private void FixedUpdate()
+    {
+        float mozeHorizontal = Input.GetAxis("Horizontal");
+        float moveVerticale = Input.GetAxis("Verticale");
+
+        Vector3 movement = new Vector3(mozeHorizontal, 0.0f, moveVerticale);
+        rb.velocity = movement;
+    }
+}
