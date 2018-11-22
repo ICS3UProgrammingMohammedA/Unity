b73ce30ab068f5c9a36eb2f0132acdb54f30f5b8
MohammedAlnajeh <43445141+MohammedAlnajeh@users.noreply.github.com>
2018-11-22 16:13:01 -0500

    edited spaceshooter

--- SpaceShooterMohammed/Assets/Scripts/PlayerController.cs
+++ SpaceShooterMohammed/Assets/Scripts/PlayerController.cs
@@ -2,21 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public class Boundary
+{
+    public float xMin, xMax, zMin, zMax;
+}
+
+
+
 public class PlayerController : MonoBehaviour
 {
+    public float speed;
+    public Boundary boundary;
     private Rigidbody rb;
-    void Start()
+
+    private void Start ()
     {
        rb  = GetComponent<Rigidbody>(); 
     }
     
 
-    private void FixedUpdate()
+    void FixedUpdate()
     {
-        float mozeHorizontal = Input.GetAxis("Horizontal");
-        float moveVerticale = Input.GetAxis("Verticale");
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVerticale = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(mozeHorizontal, 0.0f, moveVerticale);
-        rb.velocity = movement;
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVerticale);
+        rb.velocity = movement * speed;
+
+        rb.position = new Vector3
+        (
+            Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
+            0.0f,
+            Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax)
+        );
+            
+
+           
+            
     }
 }
