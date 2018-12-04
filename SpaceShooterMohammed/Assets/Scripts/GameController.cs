a0ecbc2d48149c53462d089b4659e9a675da786e
MohammedAlnajeh <43445141+MohammedAlnajeh@users.noreply.github.com>
2018-12-04 16:02:42 -0500

    added stuff

--- SpaceShooterMohammed/Assets/Scripts/GameController.cs
+++ SpaceShooterMohammed/Assets/Scripts/GameController.cs
@@ -12,14 +12,37 @@ public class GameController : MonoBehaviour
     public float waveWait;
 
     public GUIText scoreText;
+    public GUIText restartText;
+    public GUIText gameOverText;
+
+    private bool gameOver;
+    private bool restart;
     private int score;
 
     void Start()
     {
+        gameOver = false;
+        restart = false;
+        restartText.text = "";
+        gameOverText.text = "";
         score = 0;
         UpdateScore();
         StartCoroutine(SpawnWaves());
     }
+
+    void Update()
+    {
+        if (restart)
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+
+                Application.LoadLevel(Application.loadedLevel);
+
+            }
+        }
+    }
+
 
     IEnumerator SpawnWaves()
     {
@@ -34,6 +57,13 @@ public class GameController : MonoBehaviour
                 yield return new WaitForSeconds(spawnWait);
             }
             yield return new WaitForSeconds(waveWait);
+
+            if (gameOver)
+            {
+                restartText.text = "Press 'R' for Restart";
+                restart = true;
+                break;
+            }
         }
     }
 
@@ -48,4 +78,9 @@ public class GameController : MonoBehaviour
         scoreText.text = "Score: " + score;
     }
 
+    public void GameOver()
+    {
+        gameOverText.text = "Game Over!";
+        gameOver = true;
+    }
 } 
